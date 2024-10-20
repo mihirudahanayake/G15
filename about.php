000ad05cd092960f3c3b884a7587f00e64fdb620
mihirudahanayake <mihirudahanayake@gmail.com>
2024-10-21 00:07:45 +0530

    Update about.php

--- about.php
+++ about.php
@@ -28,7 +28,7 @@
                 <img id="adminImg2" src="images/bg2.jpg" alt="admin Image" class="image-popup-trigger">
                 <img id="adminImg3" src="images/bg3.jpg" alt="admin Image" class="image-popup-trigger">
                 <img id="adminImg3" src="images/bg5.jpg" alt="admin Image" class="image-popup-trigger">
-                <img id="adminImg3" src="images/bg3.jpg" alt="admin Image" class="image-popup-trigger">
+                <img id="adminImg3" src="images/bg6.jpg" alt="admin Image" class="image-popup-trigger">
                 <img id="adminImg3" src="images/bg3.jpg" alt="admin Image" class="image-popup-trigger">
             </div>
 
